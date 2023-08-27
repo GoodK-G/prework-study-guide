453a62893be34566f2e634a6671905f5c8634346
Kevin Good <kvngood65@gmail.com>
2023-08-27 17:22:15 -0500

    JavaScript and HTML changes.

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,15 @@
-//var topics = "HTML, CSS, Git, JavaScript";
+
+
+
+
+
+
 //the topics variable contains the string above that includes everything that I have been learning so far.
 // All strings are surrounded by quotation marks. and each line ends with a semicolin to signal to end the line there.
 //console.log(topics);
 
 //var topic = "JavaScript";
-/*
-if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === "CSS") {
-    console.log("Let's study CSS!");   
-} else if (topic === "Git") {
-    console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-}else {
-    console.log('Please try again!');
-}
-*/
+
 // === is a strict equality operator, this checks to see if two values are equal and returns a Boolean result (True or False)
 
 //adding an array using a for loop
@@ -29,7 +22,42 @@ for(var x = 0; x < shapes.length; x++) {
     console.log(shapes[x]);
    }
 */
-var topics = ['HTML', 'CSS', 'Git','JavaScript'];
+
+/*var topics = ['HTML', 'CSS', 'Git','JavaScript'];
 for(var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
+
+function helloWorld() {
+    console.log("Hello, world, I am a function!")
+}*/
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+
+    }
+}
+
+function selectTopic(){
+    if (randomTopic === 'HTML') {
+    console.log("Let's study HTML!");
+    } else if (randomTopic === "CSS") {
+    console.log("Let's study CSS!");   
+    } else if (randomTopic === "Git") {
+    console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+    }else {
+    console.log('Please try again!');
+    }
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+console.log('Which topic should we study first?');
+selectTopic();
